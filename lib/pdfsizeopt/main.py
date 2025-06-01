a1c2d904ba00104af752ec2a15c89a7d03f7e83d
Jordan Williams <jordan@jwillikers.com>
2025-06-01 15:43:35 -0500

    Invoke ect with multithreaded deflate enabled

--- lib/pdfsizeopt/main.py
+++ lib/pdfsizeopt/main.py
@@ -9437,8 +9437,8 @@ IMAGE_OPTIMIZER_CMD_MAP = {
     'optipng':  'optipng %(sourcefnq)s -o4 -fix -force %(optipng_gray_flags)s-out %(targetfnq)s',
     'optipng4': 'optipng %(sourcefnq)s -o4 -fix -force %(optipng_gray_flags)s-out %(targetfnq)s',
     'optipng7': 'optipng %(sourcefnq)s -o7 -fix -force %(optipng_gray_flags)s-out %(targetfnq)s',  # Slowest.
-    'ect': 'ECT -9 -strip %(targetfnq)s',
-    'ECT': 'ECT -9 -strip %(targetfnq)s',
+    'ect': 'ECT -9 -strip --mt-deflate %(targetfnq)s',
+    'ECT': 'ECT -9 -strip --mt-deflate %(targetfnq)s',
     'advpng':  'advpng -z3 -f %(targetfnq)s',
     'advpng3': 'advpng -z3 -f %(targetfnq)s',
     'advpng4': 'advpng -z4 -f %(targetfnq)s',  # Slowest, this uses Zopfli.
